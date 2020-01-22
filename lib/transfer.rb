685a5b4e3a7d822e6ca92d4477fa14df09269731
Justin Branch <justinms@outlook.com>
2020-01-22 02:43:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,15 @@
 class Transfer
-  attr_reader :sender, :receiver, :status
+  attr_reader :sender, :receiver, :status, :money
   
   def initialize(bank1, bank2, money)
     @sender = bank1
     @receiver = bank2
     @status = "pending"
+    @amount = money
+  end
+  
+  def valid?
+    @sender.valid? && @receiver.valid?
   end 
+  
 end
