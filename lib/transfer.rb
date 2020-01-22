552a396fdcf4613185134d2ac7c27b9ba6a4d331
Justin Branch <justinms@outlook.com>
2020-01-22 02:52:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,9 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       self.status = "complete"
-    end 
+    else
+      "Transaction rejected. Please check your account balance."
+    end
   end
   
     
