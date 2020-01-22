516af4f8d3e2a0de428744d24ccf293908754469
Justin Branch <justinms@outlook.com>
2020-01-22 02:47:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   def execute_transaction
     @sender.balance -= @amount
     @receiver.balance += @amount
-    Transfer.status = "complete"
+    self.class.status = "complete"
   end 
   
 end
